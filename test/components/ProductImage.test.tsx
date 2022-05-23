6c2a45f04103c8860e187b0adfa1fd1c3317afcf
mauroar1001 <83997911+mauro-arcelles@users.noreply.github.com>
2022-05-23 12:33:56 -0500

    version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+import { ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+import { ProductImage } from '../../src/components/ProductImage';
+
+
+describe('ProductImage', () => {
+
+  test('debe de mostrar el componente correctamente con la imagen personalizada', () => {
+
+    const wrapper = renderer.create(
+      <ProductImage img='https://hola.jpg' />
+    );
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+
+  });
+
+  test('debe de mostrar el componente con la imagen del producto', () => {
+
+    const wrapper = renderer.create(
+      <ProductCard product={product2}>
+        {
+          () => (
+            <ProductImage />
+          )
+        }
+      </ProductCard>
+    );
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+});
